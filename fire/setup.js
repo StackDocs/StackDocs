3b3d32e7f67c43efe610cdbdbafc467f41106cae
Domnik Mack <domnikmack@gmail.com>
2018-03-16 11:40:17 -0400

    extension shell added

--- fire/setup.js
+++ fire/setup.js
@@ -1,1 +1,14 @@
-throw new Error('fire/setup.js does not contain a config. You need to run `npm prepare`')
+module.exports = firebase =>
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+firebase.initializeApp({
+  "apiKey": "AIzaSyAKaRAd1hVazZ242hd9_u3TPEBDN8AzamQ",
+  "databaseURL": "https://chromelights-bb54c.firebaseio.com",
+  "storageBucket": "chromelights-bb54c.appspot.com",
+  "authDomain": "chromelights-bb54c.firebaseapp.com",
+  "messagingSenderId": "851772945859",
+  "projectId": "chromelights-bb54c"
+});
+
