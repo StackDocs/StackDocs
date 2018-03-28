76e7a616cfa0eb710a5dc073f47cd6a9e9441cf8
Sarah Laine <sjlaine624@gmail.com>
2018-03-28 16:19:46 -0400

    fixed unmark problem

--- chrome/src/components/SecondaryHeader.jsx
+++ chrome/src/components/SecondaryHeader.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Logout from './Logout';
 import ViewBtnBack from 'svg-react-loader?name=AnnotationIcon!~/chrome/src/icons/view-btn-back.svg';
+import { connect } from 'react-redux';
 
 
-export default function SecondaryHeader(props) {
-  return (
-    <div className="chromelights-secondary-header">
-        <p className="chromelights-back-btn" onClick={props.goToPreviousView} >	Back</p>
-      <p>Hi, {props.userDisplayName}!</p>
-      <Logout />
-    </div>
-  );
+class SecondaryHeader extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  clickBack = () => {
+    const view = this.props.currentView;
+    const highlightStr = this.props.highlightObj.newString || null;
+    if (view === 'askOrAnnotate' && highlightStr) {
+      console.log(highlightStr);
+      this.props.markInstance.unmark({className: highlightStr});
+    }
+    this.props.goToPreviousView();
+  }
+
+  render() {
+    return (
+      <div className="chromelights-secondary-header">
+          <p className="chromelights-back-btn" onClick={this.clickBack} >	Back</p>
+        <p>Hi, {this.props.userDisplayName}!</p>
+        <Logout />
+      </div>
+    );
+  }
 }
+
+const MapState = ({highlight}) => {
+  const highlightObj = highlight.highlightObj;
+  const markInstance = highlight.markInstance;
+  return { highlightObj, markInstance };
+};
+
+export default connect(MapState, null)(SecondaryHeader);
